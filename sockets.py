b737429debed1b08af728c450f849e0f4e2393fe
iris <77365934+irriss-nn@users.noreply.github.com>
2022-11-30 13:42:04 -0700

    subscribe

--- sockets.py
+++ sockets.py
@@ -25,6 +25,7 @@ import os
 app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
+clients = list()
 
 class World:
     def __init__(self):
@@ -59,33 +60,24 @@ class World:
     def world(self):
         return self.space
 
-myWorld = World()        
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+
+
+
+myWorld = World() 
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    send_all_json({entity:data})
 
-myWorld.add_set_listener( set_listener )
-        
-@app.route('/')
-def hello():
-    '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
-
-def read_ws(ws,client):
-    '''A greenlet function that reads from the websocket and updates the world'''
-    # XXX: TODO IMPLEMENT ME
-    return None
-
-@sockets.route('/subscribe')
-def subscribe_socket(ws):
-    '''Fufill the websocket URL of /subscribe, every update notify the
-       websocket and read updates from the websocket '''
-    # XXX: TODO IMPLEMENT ME
-    return None
-
-
-# I give this to you, this is how you get the raw body/data portion of a post in flask
-# this should come with flask but whatever, it's not my project.
 def flask_post_json():
     '''Ah the joys of frameworks! They do so much work for you
        that they get in the way of sane operation!'''
@@ -96,26 +88,89 @@ def flask_post_json():
     else:
         return json.loads(request.form.keys()[0])
 
+def send_all(msg):
+    for c in clients:
+        c.put(msg)
+
+def send_all_json(obj):
+    send_all(json.dumps(obj))
+
+myWorld.add_set_listener(set_listener)
+
+
+        
+@app.route('/')
+def hello():
+    '''Return something coherent here.. perhaps redirect to /static/index.html '''
+    return flask.redirect("/static/index.html")
+
+def read_ws(ws,client):
+    '''A greenlet function that reads from the websocket and updates the world'''
+    # XXX: TODO IMPLEMENT ME
+    try:
+        while True:
+            msg = ws.receive()
+            print("WS RECV: %s" % msg)
+            if (msg is not None):
+                packet = json.loads(msg)
+                for entity in packet:
+                    myWorld.set(entity, packet[entity])
+                send_all_json( packet )
+            else:
+                break
+    except:
+        pass
+
+@sockets.route('/subscribe')
+def subscribe_socket(ws):
+    '''Fufill the websocket URL of /subscribe, every update notify the
+       websocket and read updates from the websocket '''
+    # XXX: TODO IMPLEMENT ME
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client )    
+    print("Subscribing")
+    try:
+        while True:
+            # block here
+            msg = client.get()
+            print("Got a message!")
+            ws.send(msg)
+    except Exception as e:# WebSocketError as e:
+        print("WS Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
+
+
+# I give this to you, this is how you get the raw body/data portion of a post in flask
+# this should come with flask but whatever, it's not my project.
+
+
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    for k,v in flask_post_json().items():
+        myWorld.update(entity,k,v)
+
+    return myWorld.get(entity)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return myWorld.world()
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return myWorld.get(entity)
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return myWorld.world()
 
 
 
